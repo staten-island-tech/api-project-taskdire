57cd31af151bffae2390a65cd0118f61706ac084
taskdire <136046239+taskdire@users.noreply.github.com>
2024-12-09 23:05:19 -0500

    awdawd
    awdawdawd

--- app/main.js
+++ app/main.js
@@ -3,7 +3,6 @@ import "./style.css";
 const url = "https://valorant-api.com/v1/agents";
 
 async function getdata() {
-  //fetch returns a promise
   try {
     const response = await fetch(url);
     if (response.status != 200) {
@@ -30,22 +29,20 @@ function Cards(apidata) {
       `<div class="card w-1/6 h-[400px] bg-red-500 rounded-3xl flex flex-col items-center justify-evenly m-2 border-2 border-black">
       <h1 class="text-x2 text-white">${item.displayName}</h1>
         <img src="${item.fullPortraitV2}" alt="${item.displayName}" class="card-img rounded-3xl  w-50% h-50%">
-        <button class="moreinfo button bg-white" value="ore Info">
-        More Info
-      </button>
       </div>`
     );
   });
 }
-
 function filterCards(apidata) {
   Domselectors["button"].addEventListener("click", function (event) {
     let value = event.target.value;
     Domselectors.box.innerHTML = "";
     console.log(value);
     let newValue;
-    console.log(apidata.role);
-    newValue = apidata.filter((item) => item.role.displayName == value);
+
+
+    newValue = apidata.filter((item) => item.role && item.role.displayName === value);
+    console.log(newValue);
     Cards(newValue);
   });
 }
