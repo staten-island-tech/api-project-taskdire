858093d16de9030a60d61efa4e50453950a4b8ac
taskdire <136046239+taskdire@users.noreply.github.com>
2024-12-10 12:31:24 -0500

    awd
    awdawd

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import { Domselectors } from "/dom";
 import "./style.css";
 const url = "https://valorant-api.com/v1/agents";
+const tierurl = "https://valorant-api.com/v1/competitivetiers";
 
 async function getdata() {
   try {
@@ -22,6 +23,25 @@ async function getdata() {
 
 getdata();
 
+async function getTierdata() {
+  try {
+    const response = await fetch(tierurl);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      const apidata = data.data;
+      console.log(data.data);
+      getTierCards(apidata);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("Sorry, could not find this.");
+  }
+}
+
+getTierdata();
+
 function Cards(apidata) {
   apidata.forEach((item) => {
     Domselectors.box.insertAdjacentHTML(
@@ -33,16 +53,15 @@ function Cards(apidata) {
     );
   });
 }
-function filterCards(apidata) {
-  Domselectors["button"].addEventListener("click", function (event) {
-    let value = event.target.value;
-    Domselectors.box.innerHTML = "";
-    console.log(value);
-    let newValue;
 
-
-    newValue = apidata.filter((item) => item.role && item.role.displayName === value);
-    console.log(newValue);
-    Cards(newValue);
+function getTierCards(apidata) {
+  apidata.forEach((item) => {
+    Domselectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card w-1/6 h-[400px] bg-red-500 rounded-3xl flex flex-col items-center justify-evenly m-2 border-2 border-black">
+      <h1 class="text-x2 text-white">${item.tierName}</h1>
+        <img src="${item.largeIcon}" alt="${item.largeIcon}" class="card-img rounded-3xl  w-50% h-50%">
+      </div>`
+    );
   });
 }
