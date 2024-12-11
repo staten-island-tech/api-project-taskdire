bce0d60f969833bee218f455588931ee26cf3215
taskdire <136046239+taskdire@users.noreply.github.com>
2024-12-11 01:23:26 -0500

    awdawd
    awd

--- app/dom.js
+++ app/dom.js
@@ -1,6 +1,7 @@
 const Domselectors = {
   box: document.querySelector(".container"),
   button: document.querySelector(".button-container"),
+  bundlesBtn: document.getElementById("bundlesBtn"),
+  agentsBtn: document.getElementById("agentsBtn"), 
 };
-
 export { Domselectors };
--- app/main.js
+++ app/main.js
@@ -1,67 +1,77 @@
-import { Domselectors } from "/dom";
+import { Domselectors } from "./dom";
 import "./style.css";
-const url = "https://valorant-api.com/v1/agents";
-const tierurl = "https://valorant-api.com/v1/competitivetiers";
 
-async function getdata() {
+const agentsUrl = "https://valorant-api.com/v1/agents";
+const bundlesUrl = "https://valorant-api.com/v1/bundles";
+
+async function getAgentsData() {
   try {
-    const response = await fetch(url);
-    if (response.status != 200) {
+    const response = await fetch(agentsUrl);
+    if (response.status !== 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      const apidata = data.data;
-      console.log(data.data);
-      Cards(apidata);
-      filterCards(apidata);
+      const agentsData = data.data;
+      console.log("Agents Data:", agentsData);
+      Cards(agentsData);
     }
   } catch (error) {
-    console.log(error);
-    alert("Sorry, could not find this.");
+    console.error("Error fetching agents:", error);
+    alert("Sorry, could not fetch agent data.");
   }
 }
 
-getdata();
-
-async function getTierdata() {
+async function getBundlesData() {
   try {
-    const response = await fetch(tierurl);
-    if (response.status != 200) {
+    const response = await fetch(bundlesUrl);
+    if (response.status !== 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      const apidata = data.data;
-      console.log(data.data);
-      getTierCards(apidata);
+      const bundlesData = data.data;
+      console.log("Bundles Data:", bundlesData);
+      CardsForBundles(bundlesData);
     }
   } catch (error) {
-    console.log(error);
-    alert("Sorry, could not find this.");
+    console.error("Error fetching bundles:", error);
+    alert("Sorry, could not fetch bundle data.");
   }
 }
-
-getTierdata();
 
 function Cards(apidata) {
+  Domselectors.box.innerHTML = '';
   apidata.forEach((item) => {
     Domselectors.box.insertAdjacentHTML(
       "beforeend",
       `<div class="card w-1/6 h-[400px] bg-red-500 rounded-3xl flex flex-col items-center justify-evenly m-2 border-2 border-black">
-      <h1 class="text-x2 text-white">${item.displayName}</h1>
-        <img src="${item.fullPortraitV2}" alt="${item.displayName}" class="card-img rounded-3xl  w-50% h-50%">
+        <h1 class="text-xl text-white">${item.displayName}</h1>
+        <img src="${item.fullPortraitV2}" alt="${item.displayName}" class="card-img rounded-3xl w-3/4 h-1/2">
       </div>`
     );
   });
 }
 
-function getTierCards(apidata) {
-  apidata.forEach((item) => {
+function CardsForBundles(bundleData) {
+  Domselectors.box.innerHTML = '';
+  bundleData.forEach((item) => {
     Domselectors.box.insertAdjacentHTML(
       "beforeend",
-      `<div class="card w-1/6 h-[400px] bg-red-500 rounded-3xl flex flex-col items-center justify-evenly m-2 border-2 border-black">
-      <h1 class="text-x2 text-white">${item.tierName}</h1>
-        <img src="${item.largeIcon}" alt="${item.largeIcon}" class="card-img rounded-3xl  w-50% h-50%">
+      `<div class="card w-1/6 h-[400px] bg-blue-500 rounded-3xl flex flex-col items-center justify-evenly m-2 border-2 border-black">
+        <h1 class="text-xl text-white">${item.displayName}</h1>
+        <img src="${item.displayIcon}" alt="${item.displayName}" class="card-img rounded-3xl w-3/4 h-1/2">
       </div>`
     );
   });
 }
+
+Domselectors.bundlesBtn.addEventListener("click", function () {
+  Domselectors.box.innerHTML = "";
+  getBundlesData();
+});
+
+Domselectors.agentsBtn.addEventListener("click", function () {
+  Domselectors.box.innerHTML = "";
+  getAgentsData();
+});
+
+getAgentsData();
